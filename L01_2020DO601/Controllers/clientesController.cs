929de2ea872c4de66487d184f59ac83ccd13c485
Estudiante <Estudiante@B45PC05>
2023-03-04 11:56:24 -0600

    Agregar archivos de proyecto.

---
+++ L01_2020DO601/Controllers/clientesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using L01_2020DO601.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace L01_2020DO601.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class clientesController : ControllerBase
+    {
+        private readonly restauranteContext _restauranteContexto;
+
+        public clientesController(restauranteContext restauranteContexto) 
+        {
+            _restauranteContexto = restauranteContexto;
+        }
+
+        /// <summary>
+        /// EndPoint que retorna el listado
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetAll")]
+
+        public IActionResult Get() 
+        {
+            List<clientes> listadoCliente = (from e in _restauranteContexto.clientes select e).ToList();
+
+            if (listadoCliente.Count == 0) 
+            {
+                return NotFound();
+            }
+            return Ok(listadoCliente);
+        }
+    }
+}
---
+++ L01_2020DO601/Models/platos.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L01_2020DO601.Models
+{
+    public class platos
+    {
+        [Key]
+
+        public int platoId { get; set; }
+        public string nombrePlato { get; set; }
+        public decimal precio { get; set; }
+
+    }
+}
